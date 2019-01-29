8bed19dc909e29778739c7a268b8028db10cb816
Marc Ginesta <mginesta@adobe.com>
2019-01-29 20:25:46 +0100

    Filtering books on MyReads

--- src/MyReads.js
+++ src/MyReads.js
@@ -2,49 +2,49 @@ import React from "react";
 import Bookshelf from "./Bookshelf";
 import { Link } from "react-router-dom";
 
-const MyReads = props => {
-  const { books, handleChangeBookshelf } = props;
+class MyReads extends React.Component {
+  filterBooks(books, bookshelf) {
+    return books.filter(book => book.shelf === bookshelf);
+  }
 
-  const currentlyReading = books.filter(
-    book => book.shelf === "currentlyReading",
-  );
-  const wantToRead = books.filter(book => book.shelf === "wantToRead");
-  const read = books.filter(book => book.shelf === "read");
+  render() {
+    const { books, handleChangeBookshelf } = this.props;
 
-  return (
-    <div className="list-books">
-      <div className="list-books-title">
-        <h1>MyReads</h1>
-      </div>
-      <div className="list-books-content">
-        <div>
-          <Bookshelf
-            id="currentlyReading"
-            title="Currently Reading"
-            books={currentlyReading}
-            onChangeBookshelf={handleChangeBookshelf}
-          />
-          <Bookshelf
-            id="wantToRead"
-            title="Want To Read"
-            books={wantToRead}
-            onChangeBookshelf={handleChangeBookshelf}
-          />
-          <Bookshelf
-            id="read"
-            title="Read"
-            books={read}
-            onChangeBookshelf={handleChangeBookshelf}
-          />
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <Bookshelf
+              id="currentlyReading"
+              title="Currently Reading"
+              books={this.filterBooks(books, "currentlyReading")}
+              onChangeBookshelf={handleChangeBookshelf}
+            />
+            <Bookshelf
+              id="wantToRead"
+              title="Want To Read"
+              books={this.filterBooks(books, "wantToRead")}
+              onChangeBookshelf={handleChangeBookshelf}
+            />
+            <Bookshelf
+              id="read"
+              title="Read"
+              books={this.filterBooks(books, "read")}
+              onChangeBookshelf={handleChangeBookshelf}
+            />
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to="/search" className="button">
+            Add a book
+          </Link>
         </div>
       </div>
-      <div className="open-search">
-        <Link to="/search" className="button">
-          Add a book
-        </Link>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default MyReads;
