0b7e8218543511137142dab8f3443cb1685e3efa
Marc Ginesta <mginesta@adobe.com>
2019-01-29 19:41:56 +0100

    Implemented Routes, replacing state

--- src/App.js
+++ src/App.js
@@ -3,16 +3,10 @@ import * as BooksAPI from "./utils/BooksAPI";
 import "./App.css";
 import SearchBooks from "./SearchBooks";
 import MyReads from "./MyReads";
+import { Route } from "react-router-dom";
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     books: [],
   };
 
@@ -51,16 +45,19 @@ class BooksApp extends React.Component {
 
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks />
-        ) : (
-          <MyReads
-            currentlyReading={currentlyReading}
-            wantToRead={wantToRead}
-            read={read}
-            handleChangeBookshelf={this.handleChangeBookshelf}
-          />
-        )}
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <MyReads
+              currentlyReading={currentlyReading}
+              wantToRead={wantToRead}
+              read={read}
+              handleChangeBookshelf={this.handleChangeBookshelf}
+            />
+          )}
+        />
+        <Route path="/search" render={({ history }) => <SearchBooks />} />
       </div>
     );
   }
