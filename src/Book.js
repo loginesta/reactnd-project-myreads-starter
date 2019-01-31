dd8700d146cd00940aef12fa04edb74cb131739e
Marc Ginesta <mginesta@adobe.com>
2019-01-31 20:20:37 +0100

    Review Stateless vs Stateful components

--- src/Book.js
+++ src/Book.js
@@ -3,30 +3,28 @@ import BookCover from "./BookCover";
 import BookshelfChanger from "./BookshelfChanger";
 import PropTypes from "prop-types";
 
-class Book extends React.Component {
-  render() {
-    const { book, bookshelf, onChangeBookshelf } = this.props;
-    const { authors, title, imageLinks } = book;
+const Book = props => {
+  const { book, bookshelf, onChangeBookshelf } = props;
+  const { authors, title, imageLinks } = book;
 
-    return (
-      <div className="book">
-        <div className="book-top">
-          <BookCover imageLinks={imageLinks} />
-          <BookshelfChanger
-            book={book}
-            current={bookshelf}
-            onChangeBookshelf={onChangeBookshelf}
-          />
-        </div>
-        <div className="book-title">{title}</div>
-        <div className="book-authors">
-          {authors &&
-            authors.map((author, index) => <div key={index}>{author}</div>)}
-        </div>
+  return (
+    <div className="book">
+      <div className="book-top">
+        <BookCover imageLinks={imageLinks} />
+        <BookshelfChanger
+          book={book}
+          current={bookshelf}
+          onChangeBookshelf={onChangeBookshelf}
+        />
       </div>
-    );
-  }
-}
+      <div className="book-title">{title}</div>
+      <div className="book-authors">
+        {authors &&
+          authors.map((author, index) => <div key={index}>{author}</div>)}
+      </div>
+    </div>
+  );
+};
 
 Book.propTypes = {
   book: PropTypes.object.isRequired,
--- src/Booklist.js
+++ src/Booklist.js
@@ -2,24 +2,23 @@ import React from "react";
 import Book from "./Book";
 import PropTypes from "prop-types";
 
-class Booklist extends React.Component {
-  render() {
-    const { books, onChangeBookshelf } = this.props;
-    return (
-      <ol className="books-grid">
-        {books.map(book => (
-          <li key={book.id}>
-            <Book
-              book={book}
-              bookshelf={book.shelf ? book.shelf : "none"}
-              onChangeBookshelf={onChangeBookshelf}
-            />
-          </li>
-        ))}
-      </ol>
-    );
-  }
-}
+const Booklist = props => {
+  const { books, onChangeBookshelf } = props;
+
+  return (
+    <ol className="books-grid">
+      {books.map(book => (
+        <li key={book.id}>
+          <Book
+            book={book}
+            bookshelf={book.shelf ? book.shelf : "none"}
+            onChangeBookshelf={onChangeBookshelf}
+          />
+        </li>
+      ))}
+    </ol>
+  );
+};
 
 Booklist.propTypes = {
   books: PropTypes.array.isRequired,
